bdd883fd6ec6a88b5e031e5d4ccb8a2f6c7a6c01
sapnagujjar98122-lgtm <sapnagujjar98122@gmail.com>
2026-03-03 08:59:13 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-app = Client("test", bot_token="YOUR_TOKEN")
-
-@app.on_message(filters.command("start"))
-async def start(_, msg):
-    await msg.reply("Working")
-
-app.run()
